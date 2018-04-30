c8b513acdfd91568e6fca3652fec1d13b1abfa8f
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-04-30 00:09:43 -0700

    Fixed sample data entry via db:setup in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-10 times do |ilbe|
+10.times do |portfolio|
   Portfolio.create!(
-    title: "Ruby",
-    subtitle: "Rails",
-    body: "Bacon ipsum dolor amet ham hock porchetta t-bone pastrami frankfurter venison. Tail corned beef sausage tri-tip ham hock ribeye. Ribeye sirloin brisket pig cupim pastrami jowl alcatra burgdoggen shank tenderloin flank prosciutto pork belly filet mignon. Alcatra capicola beef boudin ribeye strip steak chuck tri-tip drumstick frankfurter shank.
+    title: "haho",
+    subtitle: "hoooooooo",
+    body: "nice",
+    main_image: "placehold",
+    thumb_image: "placehold"
 
-Filet mignon porchetta fatback rump andouille salami. Burgdoggen salami swine, pancetta doner pastrami tail t-bone frankfurter buffalo venison biltong pork loin rump pork chop. Turducken doner fatback, drumstick t-bone strip steak tri-tip tail tongue pig salami chicken venison chuck. Kevin tongue swine pork chop spare ribs landjaeger capicola pork sausage drumstick prosciutto leberkas short ribs tri-tip cupim. Corned beef turkey tri-tip tenderloin drumstick filet mignon bresaola cow ham hock shank rump capicola kevin brisket. Chuck cupim fatback turducken, tenderloin frankfurter strip steak drumstick tri-tip tail pig buffalo ribeye leberkas prosciutto. Beef ribs corned beef brisket ball tip cupim, ham rump andouille tail short loin ribeye bresaola.
-
-Ham hock bresaola beef ribs, short ribs buffalo turducken kielbasa tenderloin corned beef andouille. Picanha venison t-bone tri-tip sirloin leberkas beef ribs. Pig porchetta cow andouille pork loin pancetta, turducken ribeye ham hock ground round chuck. Strip steak rump burgdoggen tail, ham tenderloin boudin sausage chicken landjaeger chuck ball tip. Tenderloin pig tongue, andouille pork loin biltong pancetta jerky picanha burgdoggen pastrami landjaeger turkey filet mignon cupim.
-
-Shankle andouille ribeye picanha boudin, filet mignon short loin bacon prosciutto. Venison flank bacon doner shank. Bacon jerky buffalo brisket short ribs boudin. Beef ribs picanha prosciutto biltong pastrami tongue spare ribs ham hock short ribs landjaeger.
-
-Swine beef pig ribeye pastrami pork tail tongue t-bone turducken frankfurter biltong ham kevin corned beef. Porchetta pig ribeye jerky, capicola ball tip pastrami pork chop jowl bresaola short ribs brisket andouille strip steak burgdoggen. Turkey beef turducken, picanha hamburger shank spare ribs jerky andouille kevin. Pancetta porchetta ground round turkey strip steak pork belly. Meatloaf landjaeger cupim, strip steak tongue hamburger tail porchetta jerky beef ribs meatball pork loin ham tri-tip ground round.
-
-Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!"
   )
 end
