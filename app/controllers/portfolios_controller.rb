d5b443dec107c1b52deec4f3e831c8cf39d6c01a
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-04-29 22:47:40 -0700

    Created Portfolio controller via command line

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,19 @@
+class PortfoliosController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
