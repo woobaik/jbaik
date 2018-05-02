fe14058d766146e745c6125c2e0bbf76d21ca5b6
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-01 23:03:36 -0700

    Created create,show action controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,19 @@ class PortfoliosController < ApplicationController
   end
 
   def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html {redirect_to portfolio_path, notice: "Your fortolio item is now live."}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
+  def show
+
   end
 
   def edit
@@ -17,5 +30,11 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
   end
 end
