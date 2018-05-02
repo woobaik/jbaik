54e59e3d96319748cc08d4a4d37d9cf2a7c40c91
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-02 16:58:35 -0700

    Created default value for Portfolio image

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+
+  after_initialize :set_default
+
+  def set_default
+    self.main_image ||= "http://via.placeholder.com/350x150"
+    self.thumb_image ||= "http://via.placeholder.com/350x100"
+  end
 end
