e37b73431a4989c0017c2a149ac1703b3ab548ab
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-02 15:31:31 -0700

    Created topic.rb, and add validation. perfomed db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430053711) do
+ActiveRecord::Schema.define(version: 20180502221834) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20180430053711) do
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
