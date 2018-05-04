8a545c82758c68a77dff71eac3b01bbc9d651cfd
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-04 13:05:33 -0700

    Created custom route for ruby on rails project for portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :portfolios
   get 'angular-item', to: 'portfolios#angular'
+  get 'ruby-on-rails', to: 'portfolios#rubyonrails'
   root to: 'portfolios#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
