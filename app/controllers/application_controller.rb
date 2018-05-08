cfa0910d09ed657fe844719fa7979eb56bc215da
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-07 22:49:22 -0700

    Created devise before_action for devise, and added form field for new and edit devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter: configure_permitted_parameter, if devise_controller?
+  before_action :configure_permitted_parameter, if: :devise_controller?
 
   def configure_permitted_parameter
-    devise_parameter_sanitizer.permit(:sign_up, keys[:name])
-    devise_parameter_sanitizer.permit(:account_update, keys[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
