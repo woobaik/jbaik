276745e2fa9c77802e73a0a374d48af802bdd4e7
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-09 00:36:28 -0700

    refactored log_in register button into application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # if there is a problem showing only log in button use () +
+  def user_authenticate_button
+    if current_user
+      link_to 'Log out', destroy_user_session_path, method: :delete
+    else
+      link_to 'Register', new_user_registration_path
+      link_to 'Log In', new_user_session_path
+    end
+  end
 end
