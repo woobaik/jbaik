4a8a20647de63f7f11069901121d39117503d72c
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-09 20:19:35 -0700

    Fixed minor error 'showing only login button on page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,9 @@ module ApplicationHelper
     if current_user
       link_to 'Log out', destroy_user_session_path, method: :delete
     else
-      link_to 'Register', new_user_registration_path
-      link_to 'Log In', new_user_session_path
+      (link_to 'Register', new_user_registration_path) +
+      ('<br>'.html_safe) +
+      (link_to 'Log In', new_user_session_path)
     end
   end
 end
