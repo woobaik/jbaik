b4fec6024ec155a08dff88831f57127a96f4e16b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-10 23:22:32 -0700

    Added installation requirement for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507052814) do
+ActiveRecord::Schema.define(version: 20180511061740) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180507052814) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
